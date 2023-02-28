f7b2f0285d933db9f7b5c3e8cb4ebda03c2b265c
tewtewpemtat <tew00202@gmail.com>
2023-02-28 23:23:18 +0700

    workdone

---
+++ lab07-2023-02-24/http/src/httprequest/target/debug/deps/httprequest.d
@@ -0,0 +1,5 @@
+C:\Users\tew00\Desktop\http\src\httprequest\target\debug\deps\httprequest.exe: src\main.rs
+
+C:\Users\tew00\Desktop\http\src\httprequest\target\debug\deps\httprequest.d: src\main.rs
+
+src\main.rs:
