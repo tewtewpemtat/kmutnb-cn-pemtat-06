b4c876f4a66c12d552f01e126ede75a93f19c2fe
tewtewpemtat <tew00202@gmail.com>
2023-02-28 23:22:55 +0700

    work

--- lab07-2023-02-24/http/src/httprequest/target/debug/deps/httprequest-8f09235b68bed3c9.d
+++
@@ -1,5 +0,0 @@
-c:\Users\tew00\Desktop\http\src\httprequest\target\debug\deps\httprequest-8f09235b68bed3c9.rmeta: src\main.rs
-
-c:\Users\tew00\Desktop\http\src\httprequest\target\debug\deps\httprequest-8f09235b68bed3c9.d: src\main.rs
-
-src\main.rs:
